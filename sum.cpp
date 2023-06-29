02275f4b83bac95a52020a7a9ca43348d52139ec
tarunsheoran <118544285+tarunsheoran@users.noreply.github.com>
2023-06-29 11:53:02 +0530

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout<<"Enter first number"<<endl;
+    cin>>a;
+
+    cout<<"Enter second number"<<endl;
+    cin>>b;
+
+    cout<<"Sum is"<<a+b;
+
+return 0;
+
+}
